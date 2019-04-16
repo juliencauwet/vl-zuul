ecd075fe9ef9b6c77f036fcdf9b153914852cec7
julien <juliencauwet@yahoo.fr>
2019-04-16 14:25:53 +0200

    security works

--- src/main/java/org/greenwin/VLzuul/security/SecurityConfig.java
+++ src/main/java/org/greenwin/VLzuul/security/SecurityConfig.java
@@ -5,40 +5,16 @@ import org.greenwin.VLzuul.jwt.JwtAuthenticationConfig;
 import org.greenwin.VLzuul.jwt.JwtTokenAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletResponse;
 
-
-//@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
-        //@Autowired
-        ////@Qualifier("main")
-        //private UserDetailsService userDetailsService;
-//
-        //@Bean
-        //public UserDetailsService userDetailsService() {
-        //    return super.userDetailsService();
-        //}
-
-        //@Autowired
-        //BCryptPasswordEncoder bCryptPasswordEncoder;
-//
-        //@Override
-        //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-        //    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-        //}
 
 
     @Autowired
@@ -52,17 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
         protected void configure(HttpSecurity httpSecurity) throws Exception {
-            //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-            //http.csrf().disable();
-            //http.formLogin();
-            //http.authorizeRequests().antMatchers("/ms-users/**", "/js/**", "/images/**").permitAll();
-            //http.authorizeRequests().antMatchers("/ms-topics/**").permitAll();
-            //http.authorizeRequests().antMatchers().permitAll();
-            ////http.authorizeRequests().antMatchers("/ms-order/**",  "/images/**").authenticated();
-            //http.authorizeRequests().anyRequest().authenticated();
-            //http.addFilter(new JwtTokenAuthenticationFilter(authenticationManager()));
-            //http.addFilterBefore(new JwtTokenAuthenticationFilter(UsernamePasswordAuthenticationFilter.class));
 
             httpSecurity
                     .csrf().disable()
@@ -79,9 +44,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                             UsernamePasswordAuthenticationFilter.class)
                     .authorizeRequests()
                     .antMatchers(config.getUrl()).permitAll()
+                    .antMatchers("/ms-topics/**").hasRole("ADMIN")
                     .antMatchers("/backend/admin").hasRole("ADMIN")
                     .antMatchers("/backend/user").hasRole("USER")
                     .antMatchers("/backend/guest").permitAll();
+
 
         }
 
